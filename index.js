ef153b9949873f10383d10009889932febc8273f
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-07-23 17:34:19 +0100

    feat: http request to get coin prices

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
+const getCoinPrices = async (coin) => {
+    try {
+      const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`);
+      return response.data[coin.toLowerCase()].usd;
+    } catch (error) {
+      throw new Error('Error fetching coin prices');
+    }
+  };
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
   });
