7ad8e26f1d7f94642935d16f7c91cc4d5e5c6c55
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-07-23 18:42:46 +0100

    clean up

--- index.js
+++ index.js
@@ -1,54 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const coinRoutes = require('./src/routes');
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.json());
-
-const getCoinPrices = async (coin) => {
-    try {
-      const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`);
-      return response.data[coin.toLowerCase()].usd;
-    } catch (error) {
-      throw new Error('Error fetching coin prices');
-    }
-  };
-
-app.get('/coin/:name/price', async (req, res) => {
-    const coin = req.params.name;
-    try {
-      const price = await getCoinPrices(coin);
-      res.json({ coin, price });
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching coin price' });
-    }
-  });
-
-app.post('/coin/convert', async (req, res) => {
-    const { coin, amount } = req.body;
-    try {
-      const price = await getCoinPrices(coin);
-      const usdEquivalent = price * amount;
-      res.json({ coin, amount, usdEquivalent });
-    } catch (error) {
-      res.status(500).json({ error: 'Error converting coin to USD' });
-    }
-  });
-
-  app.post('/coin/convert/:from/:to', async (req, res) => {
-    const { from, to } = req.params;
-    const { amount } = req.body;
-  
-    try {
-      const fromPrice = await getCoinPrices(from);
-      const toPrice = await getCoinPrices(to);
-      const convertedAmount = (amount * fromPrice) / toPrice;
-      res.json({ from, to, amount, convertedAmount });
-    } catch (error) {
-      res.status(500).json({ error: 'Error converting coins' });
-    }
-  });
+app.use('/coin', coinRoutes);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
